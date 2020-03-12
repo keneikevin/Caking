0d0fab98735c1fff6d9e35a086026408a39989bb
keneikevin <kevinkenei9@gmail.com>
2020-03-12 14:57:37 +0300

    login layouts

--- app/src/main/java/com/example/caking/LoginFragment.kt
+++ app/src/main/java/com/example/caking/LoginFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.example.caking.databinding.FragmentLoginBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -15,8 +18,12 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false)
+        val binding = DataBindingUtil.inflate<FragmentLoginBinding>(inflater,
+            R.layout.fragment_login,container,false)
+        binding.log.setOnClickListener { view:View ->
+            view.findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+        return binding.root
     }
 
 }
--- app/src/main/java/com/example/caking/RegisterFragment.kt
+++ app/src/main/java/com/example/caking/RegisterFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.caking.databinding.FragmentRegisterBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -16,7 +18,9 @@ class RegisterFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        val binding = DataBindingUtil.inflate<FragmentRegisterBinding>(inflater,
+            R.layout.fragment_register,container,false)
+        return binding.root
     }
 
 }
